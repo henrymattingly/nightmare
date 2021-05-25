8be056c2aec8eda445fec4f1c6a0c7e5829d490c
Henry Mattingly <hsmattin@ucsc.edu>
2021-05-25 10:59:43 -0700

    restyle zombie class

--- src/prefabs/Zombie.js
+++ src/prefabs/Zombie.js
@@ -1,21 +1,14 @@
-class Zombie extends Phaser.Physics.Arcade.Image{
-    constructor(scene, x, y, texture, frame) {
+class Zombie extends Phaser.Physics.Arcade.Image
+{
+    constructor(scene, x, y, texture, frame) 
+    {
         super (scene, x, y, texture, frame);
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.speed = .2; 
     }
-    preload(){
-
-    }
     
-    create()
-    {
-
-    }
-    
-
     update()
     {
         if(this.y < game.config.height/2) //top
